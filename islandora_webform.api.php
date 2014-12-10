13c4ea8c3be8e45e8100c726b11093db0f907fa5
Pat Dunlavey <patdunlavey@gmail.com>
2014-12-10 17:27:06 -0500

    create hook for altering output of islandora object webform submissions page (hook_iw_results_object_submissions_page_element_alter())

---
+++ islandora_webform.api.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Modify islandora webform object submissions page.
+ *
+ * This hook enables modules to modify the output of the
+ * islandora_webform_results_object_submissions_page function, which generates
+ * the content that appears on the the submissions tab for an islandora/fedora object.
+ * The $element variable is passed by reference and may be modified by this hook.
+ *
+ * @param $element
+ *   A renderable array for the page content. It includes...
+ *    - #node: the webform object
+ *    - #object: the fedora object
+ *    - #submissions: an array of webform submission records
+ *    - table: a renderable array of the submission results
+ *
+ * @return
+ *   No value is returned.
+ */
+function hook_iw_results_object_submissions_page_element_alter(&$element) {
+  $element['table']['#prefix'] = '<h2>Blah blah</h2>';
+}
